d4bfaa57769c593d789aee844348ce209f08215c
Ryan Appel <appel@fvtc.edu>
2024-01-19 13:01:13 -0600

    starter code

---
+++ site.js
@@ -0,0 +1,22 @@
+
+
+const cardInfo1 = {
+    "title": "Nike",
+	"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "image": "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "color": "#deaf01"
+}
+
+const cardInfo2 = {
+    "title": "Independent",
+	"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "image": "https://images.pexels.com/photos/5657417/pexels-photo-5657417.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "color": "#554a33"
+}
+
+const cardInfo3 = {
+    "title": "Michael Kors",
+	"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "image": "https://images.pexels.com/photos/2783873/pexels-photo-2783873.jpeg",
+    "color": "#01322f"
+}
